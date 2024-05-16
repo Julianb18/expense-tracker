b847be89e82781534e7181f62e4c2c35fa0e9674
Julian Braun <julian141718@gmail.com>
2024-05-16 21:38:19 +0200

    pull project

--- helperFunctions/showCurrentYear.js
+++
@@ -1,9 +0,0 @@
-export const showCurrentYear = (data) => {
-  let year = {};
-  if (data && data.years) {
-    const currentYear = new Date().getFullYear();
-
-    year = data.years.find((year) => year.year === currentYear);
-  }
-  return year;
-};
